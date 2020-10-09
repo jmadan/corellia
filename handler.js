aeaf7f8ebf14c8d59fd300af15f6480677d353b2
Jasdeep Madan <v-jasdeep.madan@recordpoint.com>
2020-10-10 02:08:02 +1100

    using rollbar for logging errors

--- handler.js
+++ handler.js
@@ -2,6 +2,14 @@
 const awsObject = require("./aws");
 const service = require("./tickerService");
 const tokenService = require("./tokenService");
+const Rollbar = require("rollbar");
+
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
 
 const errorResponse = (statusCode, message) => ({
   statusCode: statusCode || 501,
@@ -29,17 +37,18 @@ module.exports.index = async (event) => {
     const { access_token, expires_in, token_type } = token;
     const { body } = Records[0];
 
-    service.getLatestPrices(body, marketStackApiKey).then(async (response) => {
-      let latestPrice = response.data;
-      if (latestPrice.close !== 0) {
-        await service.updatePrices(latestPrice, token);
-      }
+    const eodPrice = await service.getLatestPrices(body, marketStackApiKey)
+    let latestEoDPrice = eodPrice.data
+    if (latestEoDPrice.close !== 0) {
+      await service.updatePrices(latestEoDPrice, token);
+      console.log(`daily prices for symbol ${latestEoDPrice.symbol} updated successfully on ${Date.now()}`)
       return successResponse(
         201,
-        `daily prices for symbols updated successfully on ${Date.now()}`
+        `daily prices for symbol ${latestEoDPrice.symbol} updated successfully on ${Date.now()}`
       );
-    });
+    }      
   } catch (err) {
+    rollbar.log('eodOfPrices',err)
     return errorResponse(500, err);
   }
 };
